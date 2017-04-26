d2611c965142273446eab59d24dc23e4d980e884
Unknown <andrewsosa001@gmail.com>
2017-04-26 06:01:22 -0400

    Add firebase REST querying

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+var request = require('request');
+
+const url = 'https://postcred-df5cf.firebaseio.com/movies.json';
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+// Default app page
+router.get('/movies', (req, res, next) => {
+  // query movies on firebase
+  request(url, (err, response, body) => {
+    if(err) {
+      console.log(err);
+    }
+    else res.send(body);
+  });
 });
 
 module.exports = router;
