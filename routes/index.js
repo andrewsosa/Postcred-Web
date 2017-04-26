38f3bdaf727f8dd0971eec3d1d8442fca5d35b13
Unknown <andrewsosa001@gmail.com>
2017-04-26 18:02:19 -0400

    Add movie rendering

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,47 @@
 var express = require('express');
-var router = express.Router();
+var router  = express.Router();
 
 var request = require('request');
 
-const url = 'https://postcred-df5cf.firebaseio.com/movies.json';
+const storage = require('@google-cloud/storage');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const client = storage({
+  projectId: 'postcred-df5cf'
 });
 
+const bucketName = 'postcred-df5cf.appspot.com';
+const bucket = client.bucket(bucketName);
+
+const dbURL   = 'https://postcred-df5cf.firebaseio.com/movies.json';
+
 // Default app page
-router.get('/movies', (req, res, next) => {
+router.get('/', (req, res, next) => {
+
   // query movies on firebase
-  request(url, (err, response, body) => {
+  request(dbURL, (err, response, body) => {
     if(err) {
       console.log(err);
     }
-    else res.send(body);
+    else {
+      let movies = JSON.parse(body);
+      res.render('index', {movies: movies});
+    }
+
   });
+});
+
+// Get poster reference
+router.get('/images/:filename', (req, res, next) => {
+
+  let filename = req.params.filename;
+  bucket.file('images/' + filename).download().then(data => {
+    res.end(data[0], 'binary');
+  });
+
+  // var remoteReadStream = bucket.file('giraffe.jpg').createReadStream();
+  // var localWriteStream = fs.createWriteStream('/photos/zoo/giraffe.jpg');
+  // remoteReadStream.pipe(localWriteStream);
+
 });
 
 module.exports = router;
